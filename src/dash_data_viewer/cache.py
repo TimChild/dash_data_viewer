3ff15ca92a83855320d8542a0033c34e525ea062
Tim Child <timjchild@phas.ubc.ca>
2021-11-24 20:17:51 -0800

    Added documentation for cache.py

--- src/dash_data_viewer/cache.py
+++ src/dash_data_viewer/cache.py
@@ -1,3 +1,13 @@
+"""This can be imported from any page and used for caching slow functions in a fully threadsafe/process safe way
+
+from dash_data_viewer.cache import cache
+
+@cache.memoize()
+def func(args)
+    return val
+
+Note: Need to run cache.init_app(app.server) at some point if not running from multipage
+"""
 from flask_caching import Cache
 
 
@@ -5,4 +15,3 @@ cache = Cache(config={
     'CACHE_TYPE': 'filesystem',
     'CACHE_DIR': 'cache-directory'
 })
-# Note: Need to run cache.init_app(app.server) at some point
