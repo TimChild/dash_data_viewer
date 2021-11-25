81fe181c321212c9e63d14bc59a96299a26a6901
Tim Child <timjchild@phas.ubc.ca>
2021-11-24 20:17:39 -0800

    Removed old files

--- src/dash_data_viewer/multipage_run.py
+++
@@ -1,9 +0,0 @@
-from src.dash_data_viewer.multipage_util import make_multipage_app, run_app
-from dash_data_viewer.old_pages import test_page, dash_labs_templates
-
-if __name__ == '__main__':
-    multipage_app = make_multipage_app(
-        module_pages=[dash_labs_templates, test_page],
-        app_name='Dat Viewer',
-    )
-    run_app(multipage_app, debug=True, debug_port=8051, real_port=50001, threaded=True)
