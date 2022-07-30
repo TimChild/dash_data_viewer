b7710968b2692ef85a13356b7097cd50c3936760
Tim Child <48571510+TimChild@users.noreply.github.com>
2022-07-30 14:29:23 -0700

    Updates to fridge temp

--- src/dash_data_viewer/pages/fridge_temp.py
+++ src/dash_data_viewer/pages/fridge_temp.py
@@ -13,6 +13,7 @@ from dash_data_viewer.new_dat_util import get_dat
 
 from dat_analysis.new_dat.new_dat_util import get_local_config, NpEncoder
 from dat_analysis.analysis_tools.transition import CenteredAveragingProcess, TransitionFitProcess
+from dat_analysis.new_dat.dat_hdf import DatHDF
 from dat_analysis.hdf_file_handler import GlobalLock
 import dat_analysis.useful_functions as U
 import tempfile
@@ -37,15 +38,16 @@ global_persistence = 'local'
 dat_selector = c.DatSelectorAIO(multi_select=True)
 
 data_options = label_component(
-    dcc.Dropdown(id='dd-data-names', value='', options=[]), 'Data')
+    dd_opts := dcc.Dropdown(id='ft-dd-data-names', value='', options=[]), 'Data'
+)
 
 
 @callback(
-    Output('dd-data-names', 'options'),
-    Output('dd-data-names', 'value'),
+    Output('ft-dd-data-names', 'options'),
+    Output('ft-dd-data-names', 'value'),
     # Input('store-data-path', 'data'),
     Input(dat_selector.store_id, 'data'),
-    State('dd-data-names', 'value'),
+    State('ft-dd-data-names', 'value'),
 )
 def update_data_options(dat_paths, current_value):
     options = None
@@ -67,23 +69,21 @@ def update_data_options(dat_paths, current_value):
     return options, value
 
 
-g1 = c.GraphAIO(aio_id='graph-1', figure=None)
-graphs = html.Div([
+g1 = c.GraphAIO(aio_id='theta-graph', figure=None)
+theta_graphs = html.Div([
     g1,
 ])
+
+
 
 
 @callback(
     Output(g1.update_figure_store_id, 'data'),
     # Input('store-data-path', 'data'),
     Input(dat_selector.store_id, 'data'),
-    Input('dd-data-names', 'value'),
+    Input('ft-dd-data-names', 'value'),
 )
-def update_graph(data_paths, data_key) -> go.Figure():
-    # fig = go.Figure()
-    # fig.add_annotation(text=', '.join(data_paths), xref='paper', yref='paper', x=0.5, y=0.5, showarrow=False)
-    # return fig
-
+def update_thetas_graph(data_paths, data_key) -> go.Figure():
     fig = go.Figure()
     for path in data_paths:
         dat = get_dat(path)
@@ -102,47 +102,109 @@ def update_graph(data_paths, data_key) -> go.Figure():
             avg_data = averaging.outputs['averaged']
             avg_x = averaging.outputs['x']
 
-            fit = TransitionFitProcess()
-            fit.set_inputs(
+            fit_process = TransitionFitProcess()
+            fit_process.set_inputs(
                 x=avg_x,
                 transition_data=avg_data,
             )
-            fit.process()
-
-            temp = dat.Logs.temperatures['mc']*1000
-
-            fig.add_trace(go.Scatter(x=[temp], y=[fit.outputs['fits'][0].best_values.get('theta', np.nan)], name=f'Dat{dat.datnum}'))
+            fit_process.process()
+            fit = fit_process.outputs['fits']
+            if fit and fit.success:
+                temp = dat.Logs.temperatures['mc']*1000
+                fig.add_trace(go.Scatter(x=[temp], y=[fit.outputs['fits'][0].best_values.get('theta', np.nan)], name=f'Dat{dat.datnum}'))
     return fig
 
-    # dat = get_dat(data_path)
-    # if dat:
-    #     fig = go.Figure()
-    #     data = dat.Data.get_data(data_key)
-    #     x = dat.Data.x
-    #     y = dat.Data.y
-    #
-    #     fig.update_layout(
-    #         title=f'Dat{dat.datnum}: {data_key}',
-    #     )
-    #     fig.update_xaxes(title_text=dat.Logs.x_label)
-    #
-    #     if data is not None and data.ndim > 0:
-    #         x = x if x is not None else np.linspace(0, data.shape[-1], data.shape[-1])
-    #
-    #         if data.ndim == 1:
-    #             data, x = U.resample_data(data, x=x, max_num_pnts=500, resample_method='bin')
-    #             fig.add_trace(go.Scatter(x=x, y=data))
-    #         elif data.ndim == 2:
-    #             y = y if y is not None else np.linspace(0, data.shape[-2], data.shape[-2])
-    #             data, x, y= U.resample_data(data, x=x, y=y, max_num_pnts=500, resample_method='bin')
-    #             fig.update_yaxes(title_text=dat.Logs.y_label)
-    #             fig.add_trace(go.Heatmap(x=x, y=y, z=data))
-    #         else:
-    #             pass
-    #
-    #     return fig
-    # else:
-    #     return c.blank_figure()
+
+dat_graphs = html.Div(id='ft-div-dat-graphs')
+per_dat_graphs = html.Div([
+    per_dat_collapse := c.CollapseAIO(content=dat_graphs, button_text='Per Dat Graphs', start_open=True)
+])
+
+
+def check_exists(dat: DatHDF, group_path: str):
+    if dat:
+        with dat as f:
+            # if 'dash' in f.keys() and 'fridge_temp' in f['dash'].keys() and 'centering' in f['dash']['fridge_temp'].keys():
+            if group := f.get(group_path, None):
+                if group is not None:
+                    return True
+    return False
+
+
+def get_averaging(dat: DatHDF, data_key: str):
+    save_name = f'centering_{data_key.replace("/", ".")}'
+
+    averaging = None
+    with dat as f:  # Read only first
+        if check_exists(dat, f'dash/fridge_temp/{save_name}'):
+            averaging = CenteredAveragingProcess.from_hdf(f['dash']['fridge_temp'], name=save_name)
+
+    if not averaging:
+        dat.mode = 'r+'
+        with dat as f:  # Now with write mode
+            if check_exists(dat, 'dash/fridge_temp/centering'):  # In case this call was queued behind another call
+                averaging = CenteredAveragingProcess.from_hdf(f['dash']['fridge_temp'], name=save_name)
+            else:
+                averaging = CenteredAveragingProcess()
+                averaging.set_inputs(
+                    x=dat.Data.x,
+                    datas=dat.Data.get_data(data_key),
+                    center_by_fitting=True,
+                    fit_start_x=None, fit_end_x=None,
+                    initial_params=None,
+                    override_centers_for_averaging=None,
+                )
+                averaging.process(),  # TODO: Save this result to dat
+                ft_group = f.require_group('dash/fridge_temp')
+                averaging.save_to_hdf(ft_group, name='centering')
+    return averaging
+
+
+@callback(
+    Output(dat_graphs, 'children'),
+    Input(dat_selector.store_id, 'data'),
+    Input(dd_opts, 'value'),
+    State(per_dat_collapse.collapse, 'is_open'),
+)
+def update_per_dat_graphs(data_paths, data_key, open):
+    def data_with_centers_fig(x, y, data_2d, centers):
+        fig = go.Figure()
+        fig.add_trace(go.Heatmap(x=x, y=y, z=data_2d))
+        fig.add_trace(go.Scatter(x=y, y=centers, mode='markers', marker=dict(color='white')))
+        return fig
+
+    def data_with_stdev_fig(x, data, stdev):
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x=x, y=data, yerr=stdev))
+        return fig
+
+    if not open:
+        return []
+    all_entries = []
+    for path in data_paths:
+        dat = get_dat(path)
+        if dat:
+            figs = []
+            averaging = get_averaging(dat, data_key=data_key)
+
+            figs.append(data_with_centers_fig(x=averaging.inputs['x'], y=dat.Data.y, data_2d=averaging.inputs['datas'], centers=averaging.outputs['centers']))
+            figs.append(data_with_stdev_fig(x=averaging.outputs['x'], data=averaging.outputs['averaged'], stdev=averaging.outputs['std_errs']))
+
+            entry = html.Div([
+                html.H3(f'Dat{dat.datnum}'),
+                *[c.GraphAIO(figure=fig) for fig in figs],
+            ])
+
+            all_entries.append(entry)
+
+    layout = []
+    for entry in all_entries:
+        layout.append(entry)
+        layout.append(html.Hr())
+    return html.Div(children=layout)
+
+
+
 
 
 sidebar = dbc.Container([
@@ -152,7 +214,8 @@ sidebar = dbc.Container([
 )
 
 main = dbc.Container([
-    graphs,
+    per_dat_graphs,
+    theta_graphs,
 ])
 
 layout = dbc.Container([
